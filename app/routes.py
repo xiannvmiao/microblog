6df68b876fde1a26857bb1cd3b098d132eb35df7
huang <1021098080@qq.com>
2024-03-06 17:39:21 +0800

    half login

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,15 @@
-from app import app
+from app import app,db
 from flask import render_template,flash,redirect,url_for
 from app.forms import LoginForm
+from flask_login import current_user, login_user,logout_user,login_required
+from app.models import User
+import sqlalchemy as sa
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Miguel'}
+    # user = {'username': 'Miguel'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -17,13 +21,27 @@ def index():
         }
     ]
 
-    return render_template('index.html',title = 'Home', user = user, posts = posts)
+    return render_template('index.html',title = 'Home', posts = posts)
     
 @app.route('/login',methods = ['GET', 'POST'])
 def login():
 
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = LoginForm()
+
     if form.validate_on_submit():
-        flash(f'Login requested for user {form.username.data} {form.remember_me.data}')
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user,remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title = 'Sign In', form = form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
